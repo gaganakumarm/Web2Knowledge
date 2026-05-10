37681818c3f2477626c0a3436cbefe6b1b98a31e
Gagana K <gaganakumarm@gmail.com>
2026-05-10 14:57:55 +0530

    Add explicit home route

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 function extractMarkdown(scrapeResult) {
   if (!scrapeResult) return "";
 
